1ac0fec0834fb3ff43db26d565a32e15d8844fa8
Irlanda Maldonado <irly_malvel@hotmail.com>
2022-09-13 14:24:54 -0700

    Agregar archivos de proyecto.

---
+++ Practica_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int     Edad = 23;
+         string  Nombre = "Irlanda";
+         string  Pais = "Tailandia";
+
+            Console.WriteLine("Mi nombre es: {0}, tengo {1} de edad y me gustaria conocer el pais de {2}",Edad,Nombre,Pais);
+        }
+    }
+}
